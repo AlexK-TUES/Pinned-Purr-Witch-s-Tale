3773758a5411a393c2493e11d44b1b6dda03f7e0
AlexNay1383 <alexnay1383@gmail.com>
2024-04-20 00:13:20 +0300

    Added camera tracking

---
+++ Assets/Scripts/CameraFollowObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+    [SerializeField] public GameObject obj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, gameObject.transform.position.z);
+    }
+}
